a74b84d8c67b7a94f1bafe4d21058778e781bd2c
Damir Penavic <damirpenavic@gmail.com>
2024-01-31 20:53:32 +0100

    nitial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainAppComponent } from './main-app/main-app.component';
+import { AboutComponent } from './main-app/about/about.component';
+import { HomepageComponent } from './main-app/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainAppComponent,children:[
+    {path: 'about', component: AboutComponent, pathMatch: 'full'},
+    {path: 'homepage', component: HomepageComponent, pathMatch: 'full'}
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Layout/header/header.component';
+import { SidebarComponent } from './Layout/sidebar/sidebar.component';
+import { MainAppComponent } from './main-app/main-app.component';
+import { HomepageComponent } from './main-app/homepage/homepage.component';
+import { AboutComponent } from './main-app/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainAppComponent,
+    HomepageComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
