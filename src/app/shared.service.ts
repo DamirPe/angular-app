b5a89a91f0bf743f9edc81b60786fde7f79180f7
Damir Penavic <damirpenavic@gmail.com>
2024-02-02 19:47:12 +0100

    change

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -1,9 +1,6 @@
-import { Injectable } from '@angular/core';
 import { Subject, Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+
 export class SharedService {
   private isDivVisible = false;
   private visibilitySubject = new Subject<boolean>();
