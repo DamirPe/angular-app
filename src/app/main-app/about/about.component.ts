f4a7ab56cb6591adaa87ad718451c696ce9035fb
Damir Penavic <damirpenavic@gmail.com>
2024-02-06 18:49:45 +0100

    added accordion menu

--- src/app/main-app/about/about.component.ts
+++ src/app/main-app/about/about.component.ts
@@ -7,4 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  accordion1 = ['test1', 'test1', 'test1', 'test1'];
+  accordion2 = ['test2', 'test2', 'test2', 'test2'];
+  accordion3 = ['test3', 'test3', 'test3', 'test3'];
+  accordion4 = ['test4', 'test4', 'test4', 'test4'];
+  
+  acOpen = [false, false, false, false];
+
+  onAc1(){
+    for(let i = 0; i < this.acOpen.length; i++){
+      if(i == 0){
+        this.acOpen[i]=!this.acOpen[i];
+      }else{
+        this.acOpen[i]=false;
+      }
+    }
+  }
+
+  onAc2(){
+    for(let i = 0; i < this.acOpen.length; i++){
+      if(i == 1){
+        this.acOpen[i]=!this.acOpen[i];
+      }else{
+        this.acOpen[i]=false;
+      }
+    }
+  }
+
+  onAc3(){
+    for(let i = 0; i < this.acOpen.length; i++){
+      if(i == 2){
+        this.acOpen[i]=!this.acOpen[i];
+      }else{
+        this.acOpen[i]=false;
+      }
+    }
+  }
+
+  onAc4(){
+    for(let i = 0; i < this.acOpen.length; i++){
+      if(i == 3){
+        this.acOpen[i]=!this.acOpen[i];
+      }else{
+        this.acOpen[i]=false;
+      }
+    }
+  }
+
 }
